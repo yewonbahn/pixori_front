9e193f70cffa5048292937ee0eff590469c3b46b
유현선 <yoohyunsun@yuhyeonseon-ui-MacBookAir.local>
2021-05-20 00:12:58 +0900

    :sparkles: Add AuthCluster function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 
 import App from './Components/App';
 
+import "./config"
+import {AuthCluster} from './clusters/auth-cluster';
+import {RecoilRoot} from "recoil"
+import {CurrentUserSubscription} from "./hooks/current-user"
 
 ReactDOM.render(
-    <App />,document.getElementById('root'));
+  <React.StrictMode>
+    <RecoilRoot>
+      <AuthCluster />
+      <CurrentUserSubscription />
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>, document.getElementById('root'));
 
