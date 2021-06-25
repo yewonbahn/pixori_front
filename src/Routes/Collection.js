fdd426527b6611f6c9e8d122660a57d9b62bb482
유현선 <yoohyunsun@MacBook-Pro.local>
2021-06-25 20:38:59 +0900

    :sparkles: Add ErrorTokenCluster function

--- src/Routes/Collection.js
+++ src/Routes/Collection.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {TokenCluster} from '../clusters/token-cluster'
+import {ErrorTokenCluster} from '../clusters/error-token-cluster'
 import {useCurrentUser} from '../hooks/current-user'
 
 export default () => {
 
 function Token() {
     const cu = useCurrentUser()
-    return (
-      <TokenCluster address={cu.addr} />
-    )
+
+    if (cu.addr){
+      return (
+        <TokenCluster address={cu.addr} />
+      )
+    }
+    else{
+      return(
+      <ErrorTokenCluster />
+      )
+    }
   }
 
   return (
---
+++ src/clusters/error-token-cluster.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function ErrorTokenCluster() {
+  return (
+    <div>
+      <p>
+        [error page] log in to your account
+      </p>
+    </div>
+  );
+}
